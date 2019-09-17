6c1430e432191942e44d0f6296fd9c66de6673d1
Daniel V <davarg@harrisonburg.k12.va.us>
2019-09-17 09:48:41 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,18 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('-', btnPress2);
+createButton('+', btnPress); // Make a button
 changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
+
 
 function btnPress() {
   score++;
   postToPage(score);
 }
 
+function btnPress2() {
+  score--;
+  postToPage(score);
+}
