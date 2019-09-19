9b8bcca1b195e3185ff111db82c30d17621192ad
Daniel V <davarg@harrisonburg.k12.va.us>
2019-09-19 09:44:18 -0400

    Beta V0.0.1

--- game.js
+++ game.js
@@ -1,21 +1,34 @@
-var score = 1; // Create a variable to store the score in
+let score = 0; // Create a variable to store the score in
+let button2 = new Button('+ 2x', btnPress3);
+button2.hide()
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
+var title = new Title('Troll'); // Create a title element
+var button = new Button('+', btnPress);   // Create a button element
+var button1 = new Button('-', btnPress2);
+//var button = new Button('+ 2x',btnPress3);
+//button.hide()
 var scr = new Text(score);                    // Create a text element
-
-createButton('-', btnPress2);
-createButton('+', btnPress); // Make a button
-changeTitle('Clicker Game');
-
 
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
+  if(score > 20) {
+    button2.show();
+  }
 }
 
 function btnPress2() {
   score--;
-  postToPage(score);
+  scr.edit(score);
+  if(score > 20) {
+    button2.show();
+  }
 }
+
+function btnPress3(){
+  score = 0;
+  scr.edit(score);
+  button2.hide()
+}
+
